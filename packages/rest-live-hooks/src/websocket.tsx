458296112cd902f6750912d8842ced17698884e3
Ying Xuan <engyingxuan@gmail.com>
2020-09-28 20:43:36 -0400

    modified websocket

--- packages/rest-live-hooks/src/websocket.tsx
+++ packages/rest-live-hooks/src/websocket.tsx
@@ -1,30 +1,58 @@
-import { ResourceUpdate, SubscribeRequest } from "./types";
+import React, {
+  useRef,
+  MutableRefObject,
+  createContext,
+  ReactNode,
+  useState,
+} from "react";
+import ReconnectingWebSocket from "reconnecting-websocket";
+import { ResourceUpdate, RevalidationUpdate, SubscribeRequest } from "./types";
 import { Identifiable } from "@pennlabs/rest-hooks";
-import { MutableRefObject } from "react";
 import { SITE_ORIGIN } from "./findOrigin";
 
 type UpdateListener = {
   request: SubscribeRequest<any>;
   notify: MutableRefObject<
-    (update: ResourceUpdate<any>) => Promise<any[] | any>
+    (update: ResourceUpdate<any> | RevalidationUpdate) => Promise<any[] | any>
   >;
   uuid: number;
 };
 
+export type RLHContextProps = {
+  websocket: WebsocketManager;
+  isConnected: boolean;
+};
+
+export const RLHContext = createContext<RLHContextProps | undefined>(undefined);
+
+export function RLHInstance({ children }: { children: ReactNode }) {
+  const [isConnected, setIsConnected] = useState(true);
+  const websocketRef = useRef(
+    new WebsocketManager("/api/ws/subscribe/", setIsConnected)
+  );
+
+  return (
+    <RLHContext.Provider
+      value={{ websocket: websocketRef.current, isConnected }}
+    >
+      {children}
+    </RLHContext.Provider>
+  );
+}
+
 class WebsocketManager {
   private listeners: UpdateListener[];
-  private websocket: WebSocket | null;
+  private websocket: ReconnectingWebSocket | null;
   private connectionComplete: Promise<boolean>;
   private setConnected: (value?: boolean | PromiseLike<boolean>) => void;
   private readonly url: string;
+  private isConnectedCallback: (isConnected: boolean) => any;
 
   connect(): Promise<void> {
     const url = SITE_ORIGIN() + this.url;
-    return new Promise<void>((resolve, reject) => {
-      this.websocket = new WebSocket(url);
-      this.websocket.onerror = () => {};
-      this.websocket.onclose = () => {};
-      this.websocket.onmessage = (event: MessageEvent) => {
+    return new Promise<void>((resolve, _reject) => {
+      this.websocket = new ReconnectingWebSocket(url);
+      this.websocket.addEventListener("message", (event: MessageEvent) => {
         const message = JSON.parse(event.data);
         if (message.model) {
           const update = message as ResourceUpdate<any>;
@@ -37,31 +65,69 @@ class WebsocketManager {
             listener.notify.current(update)
           );
         }
-      };
-      this.websocket.onopen = () => {
+      });
+
+      // Closed/Error event handlers set connectionComplete to be a promise
+      // that won't resolve until the connection is reopened to prevent
+      // subscriptions from going through until the connection has been opened.
+      //
+      // NOTE: This is necessary since the subscribe request will fail
+      // since it calls send directly on the socket connection which will
+      // throw an error.
+
+      this.websocket.addEventListener("error", () => {
+        this.isConnectedCallback(false);
+        this.connectionComplete = new Promise<boolean>((resolve) => {
+          this.setConnected = resolve;
+        });
+      });
+
+      this.websocket.addEventListener("close", () => {
+        this.isConnectedCallback(false);
+        this.connectionComplete = new Promise<boolean>((resolve) => {
+          this.setConnected = resolve;
+        });
+      });
+
+      this.websocket.addEventListener("open", () => {
+        // NOTE: This operates under the assumption that no subscription will
+        // arrive before open is done iterating through the listeners, since
+        // buffered messages will be sent twice otherwise
+
+        this.listeners.forEach((listener) => {
+          listener.notify.current({ action: "REVALIDATE" });
+          // if websocket is open then it cannot be null
+          this.websocket!.send(JSON.stringify(listener.request));
+        });
+        this.isConnectedCallback(true);
         this.setConnected(true);
         resolve();
-      };
+      });
     });
   }
-  constructor(url: string) {
+  constructor(url: string, isConnectedCallback: (isConnected: boolean) => any) {
     this.listeners = [];
     this.websocket = null;
     this.url = url;
     this.connectionComplete = new Promise<boolean>((resolve) => {
       this.setConnected = resolve;
     });
+    this.isConnectedCallback = isConnectedCallback;
   }
 
   reset() {
     this.listeners = [];
-    this.websocket.close();
-    this.websocket = null;
+    if (this.websocket) {
+      this.websocket.close();
+      this.websocket = null;
+    }
   }
 
   async subscribe<T extends Identifiable>(
     request: SubscribeRequest<T>,
-    notify: MutableRefObject<(update: ResourceUpdate<T>) => Promise<T[] | T>>,
+    notify: MutableRefObject<
+      (update: ResourceUpdate<T> | RevalidationUpdate) => Promise<T[] | T>
+    >,
     uuid: number
   ) {
     if (this.websocket === null) {
@@ -69,7 +135,9 @@ class WebsocketManager {
     } else {
       await this.connectionComplete;
     }
-    this.websocket.send(JSON.stringify(request));
+
+    // this.websocket is non-null since connection has completed
+    this.websocket!.send(JSON.stringify(request));
     this.listeners.push({
       request: request,
       notify,
@@ -78,7 +146,17 @@ class WebsocketManager {
   }
 
   unsubscribe(uuid: number) {
-    const request = this.listeners.find((l) => l.uuid === uuid).request;
+    if (!this.websocket) {
+      throw new Error(
+        "Unsubscribe cannot be called if no connection has been established"
+      );
+    }
+    const listener = this.listeners.find((l) => l.uuid == uuid);
+    if (!listener) {
+      throw new Error("invalid uuid");
+    }
+
+    const request = listener.request;
     this.websocket.send(JSON.stringify({ ...request, unsubscribe: true }));
     this.listeners = this.listeners.filter((l) => l.uuid !== uuid);
   }
@@ -88,4 +166,3 @@ export const takeTicket = (() => {
   let count = 0;
   return () => count++;
 })();
-export const websocket = new WebsocketManager("/api/ws/subscribe/");
